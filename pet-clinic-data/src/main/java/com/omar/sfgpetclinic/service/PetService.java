41cae7d86120adb37f865201384fa40e1ecbf199
imomark <khanomarkhalid@gmail.com>
2020-09-12 11:46:22 +0530

    refactoring to multi module project.

---
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/PetService.java
@@ -0,0 +1,12 @@
+package com.omar.sfgpetclinic.service;
+
+import com.omar.sfgpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/VetService.java
@@ -0,0 +1,12 @@
+package com.omar.sfgpetclinic.service;
+
+import com.omar.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}

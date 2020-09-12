0789d58979ec49a317950fc9e955485bbd8d6a93
imomark <khanomarkhalid@gmail.com>
2020-09-12 18:18:02 +0530

    refactoring service interface. Closes #5

--- pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/OwnerService.java
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/OwnerService.java
@@ -2,12 +2,8 @@ package com.omar.sfgpetclinic.service;
 
 import com.omar.sfgpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
--- pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/VetService.java
@@ -2,11 +2,6 @@ package com.omar.sfgpetclinic.service;
 
 import com.omar.sfgpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet vets);
-    Set<Vet> findAll();
 }

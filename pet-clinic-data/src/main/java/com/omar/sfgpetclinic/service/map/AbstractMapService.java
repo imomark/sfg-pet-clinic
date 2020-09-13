572b0cb19d90840c0062ac88d77dc791bf3f3290
imomark <khanomarkhalid@gmail.com>
2020-09-13 11:44:20 +0530

    Done id generation. Closes #14

--- pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/map/AbstractMapService.java
@@ -1,19 +1,27 @@
 package com.omar.sfgpetclinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.omar.sfgpetclinic.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends  Long> {
+
+    protected Map<Long, T> map = new HashMap<>();
 
     T findById(ID id){
         return map.get(id);
     }
-    T save(ID id,T object){
-        map.put(id, object);
+    T save(T object){
+
+        if (object != null){
+            if (object.getId() == null){
+                object.setId(getNextId());
+            }
+            map.put(object.getId(), object);
+        }
+        else{
+            throw  new RuntimeException("Object cannot be null");
+        }
         return object;
     }
     Set<T> findAll(){
@@ -24,5 +32,17 @@ public abstract class AbstractMapService<T, ID> {
     }
     void deleteById(ID id){
         map.remove(id);
+    }
+
+    private Long getNextId(){
+
+        Long nextId = null;
+        try{
+            nextId = Collections.max(map.keySet()) +1;
+        }
+        catch (NoSuchElementException ex){
+            nextId = 1L;
+        }
+        return nextId;
     }
 }

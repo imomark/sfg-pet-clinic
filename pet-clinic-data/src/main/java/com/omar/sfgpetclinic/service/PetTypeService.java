7cfae11586d8d5d6ca37d5275781f5643b32e67f
imomark <khanomarkhalid@gmail.com>
2020-09-14 10:07:09 +0530

    pet type Service Created. Closes #23

---
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.omar.sfgpetclinic.service;
+
+import com.omar.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/map/PetTypeMapService.java
@@ -0,0 +1,35 @@
+package com.omar.sfgpetclinic.service.map;
+
+import com.omar.sfgpetclinic.model.PetType;
+import com.omar.sfgpetclinic.service.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}

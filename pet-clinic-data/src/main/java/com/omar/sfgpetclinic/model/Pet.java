8729bbb98d9ad79c045e24a2c4f4a2ccea9158b4
imomark <khanomarkhalid@gmail.com>
2020-09-22 18:32:00 +0530

    Jpa added. Closes #32

--- pet-clinic-data/src/main/java/com/omar/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.omar.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,8 +22,20 @@ public class Pet extends  BaseEntity{
     @Column(name = "name")
     private String name;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+
     public String getName() {
         return name;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 
     public void setName(String name) {

a713ac1e8a7c49cdd99954e373f38f9e58f10e2c
imomark <khanomarkhalid@gmail.com>
2020-09-22 18:11:20 +0530

    Jpa added. Closes #31

--- pet-clinic-data/src/main/java/com/omar/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/model/Speciality.java
@@ -1,7 +1,15 @@
 package com.omar.sfgpetclinic.model;
 
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {

d9b54ac165c9e443a69e1b688ffff9b11b16f31a
imomark <khanomarkhalid@gmail.com>
2020-09-24 16:04:32 +0530

    Jpa added. Closes #37

---
+++ pet-clinic-data/src/main/java/com/omar/sfgpetclinic/service/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,48 @@
+package com.omar.sfgpetclinic.service.springdatajpa;
+
+import com.omar.sfgpetclinic.model.Speciality;
+import com.omar.sfgpetclinic.repositories.SpecialityRepository;
+import com.omar.sfgpetclinic.service.SpecialitiesService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialitiesService {
+
+    private final SpecialityRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
